6fa1d7d950387a88aa219da2a37c8c3aaabddcfc
Samuel Berwanger <samuelbwr@gmail.com>
2017-12-27 22:18:26 -0200

    building correctly

---
+++ src/main/java/samuelbwr/upapi/UpApiApplication.java
@@ -0,0 +1,12 @@
+package samuelbwr.upapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpApiApplication.class, args);
+	}
+}
